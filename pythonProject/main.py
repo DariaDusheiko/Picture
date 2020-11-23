5bc614cea12387cbd634d3291e9bf6ed32ca6ca2
DariaDusheiko <dush19122004@gmail.com>
2020-11-24 00:02:58 +0300

    2

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageDraw
 import sys
+import random
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 import sys  #  загружаем библиотеки и различные модули
@@ -24,23 +25,25 @@ class MyWidget(QMainWindow):
         self.pushButton.clicked.connect(self.run1)  # делаем возможность нажатия на кнопку, перенаправляет нас в функцию
         self.pushButton.setStyleSheet("background-color: rgb(153, 50, 204);")  # изменяем цвет кнопки
 
-    def run1(self, sky_color='#87CEEB',
-            ocean_color='#017B92', boat_color='#874535',
-            sail_color='#FFFFFF', sun_color='#FFCF40'):  # создаём функцию, которой при входе не задаются аргументы
+    def run1(self):  # создаём функцию, которой при входе не задаются аргументы
         im = Image.new("RGB", (500, 500))
+        k = ['#87CEEB', '#017B92', '#874535', '#FFFFFF', '#FFCF40']
+        p = random.randint(1, 5)
         drawer = ImageDraw.Draw(im)
         drawer.ellipse((
             (int(0.8 * 500), -int(0.2 * 500)),
             (int(1.2 * 400), int(0.2 * 500))),
-            sun_color)
+            k[p])
+        p = random.randint(0, 4)
         drawer.ellipse((
             (int(0.8 * 250), -int(0.2 * 500)),
             (int(1.2 * 250), int(0.2 * 500))),
-            sun_color)
+            k[p])
+        p = random.randint(1, 5)
         drawer.ellipse((
             (int(0.8 * 100), -int(0.2 * 50)),
             (int(1.2 * 100), int(0.2 * 50))),
-            sun_color)
+            k[p])
         im.save('res1.jpg')  # сохраняем результат
         self.pixmap = QPixmap('res1.jpg')  # загружаем картинку
         self.label.setPixmap(self.pixmap)  # открываем картинку в label
